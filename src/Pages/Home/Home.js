f0571c8519b329ad628dd44f0509763d5b1ee673
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-05-12 10:47:56 +0600

    card

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import Info from "./Info";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Info></Info>
       <h3>This is home</h3>
     </div>
   );
